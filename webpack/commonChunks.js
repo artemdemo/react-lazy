bb99f3f46336ed910c6540ef687da0395a3140c1
Artem Demo <artemdemo@gmail.com>
2017-11-19 07:44:22 +0200

    reorgonized commonChunks

--- webpack/commonChunks.js
+++ webpack/commonChunks.js
@@ -1,14 +1,11 @@
-const CommonsChunkPlugin = require('webpack').optimize.CommonsChunkPlugin;
+const { CommonsChunkPlugin } = require('webpack').optimize;
 
 const staticCommons = (prod = false) => {
     const filename = prod ? './js/common-[chunkhash].js' : './js/common.js';
     return new CommonsChunkPlugin({
         name: 'common',
         filename,
-        minChunks(module, count) {
-            const context = module.context;
-            return context && context.indexOf('node_modules') >= 0;
-        },
+        minChunks: Infinity,
     });
 };
 
