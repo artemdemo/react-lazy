428629ec8800e644dd4ec260c3093d8382a5d278
Artem Demo <artemdemo@gmail.com>
2018-12-01 16:40:20 +0200

    reoranged compoanents

--- source/views/DashboardView.jsx
+++ source/views/DashboardView.jsx
@@ -1,56 +1,33 @@
+/* eslint-disable react/prefer-stateless-function */
 import React from 'react';
 import LastOrders from '../containers/LastOrders/LastOrder.async';
 import OrdersMap from '../containers/OrdersMap/OrdersMap.async';
 import StoresIncome from '../containers/StoresIncome/StoresIncome.async';
 import OrdersByCity from '../containers/OrdersByCity/OrdersByCity.async';
 
-class DashboardView extends React.Component {
-    renderLastOrders() {
-        return (
-            <LastOrders />
-        );
-    }
-
-    renderOrdersMap() {
-        return (
-            <OrdersMap />
-        );
-    }
-
-    renderOrdersByCity() {
-        return (
-            <OrdersByCity>
-                Custom loading ....
-            </OrdersByCity>
-        );
-    }
-
-    renderStoresIncome() {
-        return (
-            <StoresIncome />
-        );
-    }
-
+class DashboardView extends React.PureComponent {
     render() {
         return (
-            <div>
+            <React.Fragment>
                 <div className='row'>
                     <div className='col-md-4'>
-                        {this.renderLastOrders()}
+                        <LastOrders />
                     </div>
                     <div className='col-md-8'>
-                        {this.renderOrdersMap()}
+                        <OrdersMap />
                     </div>
                 </div>
                 <div className='row'>
                     <div className='col-md-4'>
-                        {this.renderOrdersByCity()}
+                        <OrdersByCity>
+                            Custom loading ....
+                        </OrdersByCity>
                     </div>
                     <div className='col-md-8'>
-                        {this.renderStoresIncome()}
+                        <StoresIncome />
                     </div>
                 </div>
-            </div>
+            </React.Fragment>
         );
     }
 }
